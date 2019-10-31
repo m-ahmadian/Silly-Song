4b03704affa051d7eeb19adda35807a8247b8ef6
Mehrdad <Mehrdad@Mehrdads-iMac.local>
2019-10-31 06:55:42 +0330

    Add outlets, actions and lyrics template to the UIViewController

--- Silly Song/ViewController.swift
+++ Silly Song/ViewController.swift
@@ -8,17 +8,69 @@
 import Foundation
 import UIKit
 
+let bananaFanaTemplate = [
+    "<FULL_NAME>, <FULL_NAME>, Bo B<SHORT_NAME>",
+    "Banana Fana Fo F<SHORT_NAME>",
+    "Me My Mo M<SHORT_NAME>",
+    "<FULL_NAME>"
+].joined(separator: "\n")
+
+
+extension ViewController: UITextFieldDelegate {
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        textField.resignFirstResponder()
+        return false
+    }
+}
+
 
 class ViewController: UIViewController {
 
+    
+    // MARK: Outlets
+    @IBOutlet weak var textField: UITextField!
+    @IBOutlet weak var textView: UITextView!
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        self.textField.delegate = self
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    
+    
+    // MARK: Actions
+    
+    @IBAction func reset(_ sender: Any) {
+        self.textField.text = ""
+        self.textView.text = ""
+    }
+    
+    
+    @IBAction func displayLyrics(_ sender: Any) {
+        self.textView.text = lyricsForTemplate(lyricsTemplate: bananaFanaTemplate, fullName: textField.text ?? "")
+    }
+    
+}
 
+
+// MARK: Functions
+
+func shortNameFromName(name: String) -> String {
+    let lowerName = name.lowercased()
+    let vowelSet = CharacterSet(charactersIn: "aeiou")
+    let index = lowerName.rangeOfCharacter(from: vowelSet)!.lowerBound
+    return String(lowerName[index...])
+}
+
+func lyricsForTemplate(lyricsTemplate: String, fullName: String) -> String {
+    let shortName = shortNameFromName(name: fullName)
+    var lyrics = lyricsTemplate.replacingOccurrences(of: "<SHORT_NAME>", with: shortName)
+    lyrics = lyrics.replacingOccurrences(of: "<FULL_NAME>", with: fullName)
+    return lyrics
 }
