be1b6c7c74db4ba03bcf40bcf708221c06a7fa65
Mehrdad <Mehrdad@Mehrdads-iMac.local>
2019-10-31 05:54:27 +0330

    Create the UIView for the Silly Song

---
+++ Silly Song/ViewController.swift
@@ -0,0 +1,24 @@
+//
+//  ViewController.swift
+//  Silly Song
+//
+//  Created by Mehrdad on 8/15/18.
+//  Copyright © 2018 Mehrdad. All rights reserved.
+//
+import Foundation
+import UIKit
+
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+}
